fe7a8fedf0f78354f0d4bf01c66f4edc3b35930b
Kamil Pierudzki <kamil.pierudzki@paramount.com>
2024-10-16 12:10:30 +0200

    Create CalculateNewTextureSizeTest

--- app/src/main/java/tv/pluto/dynamic/cropping/android/logic/CalculateNewTextureSize.kt
+++ app/src/main/java/tv/pluto/dynamic/cropping/android/logic/CalculateNewTextureSize.kt
@@ -5,12 +5,11 @@ import kotlin.math.roundToInt
 class CalculateNewTextureSize {
 
     fun calculate(videoResolution: VideoResolution, textureViewSize: Size): TextureSize {
-        val aspectRatio = (1.0 * videoResolution.value.width.value) / videoResolution.value.height.value
-        return TextureSize(
-            Size(
-                Width((1.0 * textureViewSize.height.value * aspectRatio).roundToInt()),
-                Height((1.0 * textureViewSize.width.value * aspectRatio).roundToInt()),
-            )
-        )
+        if (videoResolution.value.height.value == 0) {
+            return TextureSize(Size(Width(1), Height(1)))
+        }
+        val scale = textureViewSize.height.value.toFloat() / videoResolution.value.height.value.toFloat()
+        val newWidth = (videoResolution.value.width.value * scale).roundToInt()
+        return TextureSize(Size(Width(newWidth), Height(textureViewSize.height.value)))
     }
 }
--- app/src/test/java/tv/pluto/dynamic/cropping/android/logic/CalculateNewTextureSizeTest.kt
+++ app/src/test/java/tv/pluto/dynamic/cropping/android/logic/CalculateNewTextureSizeTest.kt
@@ -1,5 +1,64 @@
 package tv.pluto.dynamic.cropping.android.logic
 
-class CalculateNewTextureSizeTest { // todo
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThrows
+import org.junit.Test
 
+class CalculateNewTextureSizeTest {
+
+    @Test
+    fun test_1() {
+        // given
+        val videoResolution = VideoResolution(Size(Width(1920), Height(1080))) // 16:9
+        val textureViewSize = Size(Width(1280), Height(720))  // 16:9
+
+        // when
+        val result = CalculateNewTextureSize().calculate(videoResolution, textureViewSize)
+
+        // then
+        assertEquals(1280, result.value.width.value)
+        assertEquals(720, result.value.height.value)
+    }
+
+    @Test
+    fun test_2() {
+        // given
+        val videoResolution = VideoResolution(Size(Width(1024), Height(768)))  // 4:3
+        val textureViewSize = Size(Width(1280), Height(720))  // 16:9
+
+        // when
+        val result = CalculateNewTextureSize().calculate(videoResolution, textureViewSize)
+
+        // then
+        assertEquals(960, result.value.width.value)  // 720 * (1024 / 768) = 960
+        assertEquals(720, result.value.height.value)
+    }
+
+    @Test
+    fun test_3() {
+        // given
+        val videoResolution = VideoResolution(Size(Width(1920), Height(0)))
+        val textureViewSize = Size(Width(1280), Height(720))
+
+        // when
+        val result = CalculateNewTextureSize().calculate(videoResolution, textureViewSize)
+
+        // then
+        assertEquals(1, result.value.width.value)  // 720 * (1024 / 768) = 960
+        assertEquals(1, result.value.height.value)
+    }
+
+    @Test
+    fun test_4() {
+        // given
+        val videoResolution = VideoResolution(Size(Width(640), Height(480)))
+        val textureViewSize = Size(Width(1920), Height(1080))
+
+        // when
+        val result = CalculateNewTextureSize().calculate(videoResolution, textureViewSize)
+
+        // then
+        assertEquals(1440, result.value.width.value)  // 1080 * (640 / 480) = 1440
+        assertEquals(1080, result.value.height.value)
+    }
 }
