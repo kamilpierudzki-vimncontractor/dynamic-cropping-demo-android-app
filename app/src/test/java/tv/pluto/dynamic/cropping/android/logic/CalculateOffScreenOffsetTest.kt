596c08145fc56112cc7445665372ecdb9630b42e
Kamil Pierudzki <kamil.pierudzki@paramount.com>
2024-10-16 12:36:12 +0200

    update unit test

--- app/src/test/java/tv/pluto/dynamic/cropping/android/logic/CalculateOffScreenOffsetTest.kt
+++ app/src/test/java/tv/pluto/dynamic/cropping/android/logic/CalculateOffScreenOffsetTest.kt
@@ -2,21 +2,6 @@ package tv.pluto.dynamic.cropping.android.logic
 
 import org.junit.Test
 
-class CalculateOffScreenOffsetTest {
+class CalculateOffScreenOffsetTest { // todo
 
-    private val tested = CalculateOffScreenOffset()
-
-    @Test
-    fun test_1() {
-        // given
-        val coordinate = 2.0
-        val textureViewWidth = 6
-        val textureSize = TextureSize(Size(Width(8), Height(1)))
-
-        // when
-        val actual = tested.calculated(coordinate, textureViewWidth, textureSize)
-
-        // then
-//        Assert.assertEquals()
-    }
 }
