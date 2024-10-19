bf070405318f50a78e11bd868040875acc367c2b
Kamil Pierudzki <kamil.pierudzki@paramount.com>
2024-10-19 10:28:45 +0200

    update behaviour of scrolling

--- app/src/main/java/tv/pluto/dynamic/cropping/android/framework/demo/ui/HomeScreen.kt
+++ app/src/main/java/tv/pluto/dynamic/cropping/android/framework/demo/ui/HomeScreen.kt
@@ -9,17 +9,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import kotlinx.coroutines.launch
 import tv.pluto.dynamic.cropping.android.framework.VideoPlaybackViewModel
 
 private val alpha = 0.7f
@@ -46,19 +40,11 @@ fun HomeScreen(videoPlaybackViewModel: VideoPlaybackViewModel) {
                     .fillMaxSize()
                     .background(color = Color.Black),
             ) {
-                var selectedItemIndex by rememberSaveable { mutableIntStateOf(0) }
+
                 val lazyListState = rememberLazyListState()
-                val coroutineScope = rememberCoroutineScope()
                 val videoPlayingStates by videoPlaybackViewModel.videoPlayingStates
                 val videoPositionStates by videoPlaybackViewModel.videoPositionStates
                 val coordinateIndicesStates by videoPlaybackViewModel.consumedCoordinateIndicesStates
-
-                LaunchedEffect(selectedItemIndex) {
-                    videoPlaybackViewModel.onIndexOfPlayingComponentChanged(selectedItemIndex)
-                    coroutineScope.launch {
-                        lazyListState.animateScrollToItem(selectedItemIndex)
-                    }
-                }
 
                 VideosListComponent(
                     videos = videoPlaybackViewModel.videos,
@@ -75,13 +61,8 @@ fun HomeScreen(videoPlaybackViewModel: VideoPlaybackViewModel) {
                             consumedIndexOfCoordinate,
                         )
                     },
-                    onVideoEnded = {
-                        val nextIndex = if (selectedItemIndex + 1 < videoPlaybackViewModel.videos.size) {
-                            selectedItemIndex + 1
-                        } else {
-                            0
-                        }
-                        selectedItemIndex = nextIndex
+                    onSelectedItemIndexChanged = { newSelectedItemIndex ->
+                        videoPlaybackViewModel.onIndexOfPlayingComponentChanged(newSelectedItemIndex)
                     },
                     modifier = Modifier.matchParentSize()
                 )
@@ -90,37 +71,11 @@ fun HomeScreen(videoPlaybackViewModel: VideoPlaybackViewModel) {
                         .matchParentSize()
                         .background(brush = Brush.verticalGradient(gradientOnVideos))
                 )
-                SwipeableBox(
-                    onSwipe = { direction ->
-                        coroutineScope.launch {
-                            val nextIndex = when (direction) {
-                                SwipeDirection.Down -> {
-                                    if (selectedItemIndex + 1 < videoPlaybackViewModel.videos.size) {
-                                        selectedItemIndex + 1
-                                    } else {
-                                        return@launch
-                                    }
-                                }
-
-                                SwipeDirection.Up -> {
-                                    if (selectedItemIndex - 1 >= 0) {
-                                        selectedItemIndex - 1
-                                    } else {
-                                        return@launch
-                                    }
-                                }
-                            }
-                            selectedItemIndex = nextIndex
-                        }
-                    },
-                    modifier = Modifier.matchParentSize()
-                )
                 TopSelectionComponent(
                     modifier = Modifier
                         .fillMaxWidth()
                         .align(Alignment.TopStart),
                 )
-
                 BottomBarComponent(
                     modifier = Modifier
                         .fillMaxWidth()
--- app/src/main/java/tv/pluto/dynamic/cropping/android/framework/demo/ui/VideosListComponent.kt
+++ app/src/main/java/tv/pluto/dynamic/cropping/android/framework/demo/ui/VideosListComponent.kt
@@ -6,9 +6,17 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import tv.pluto.dynamic.cropping.android.framework.Video
 
 @Composable
@@ -20,10 +28,12 @@ fun VideosListComponent(
     lazyListState: LazyListState,
     onVideoPositionChanged: (Int, Long) -> Unit,
     onCoordinateIndexConsumed: (Int, Int) -> Unit,
-    onVideoEnded: (Int) -> Unit,
+    onSelectedItemIndexChanged: (Int) -> Unit,
     modifier: Modifier = Modifier,
 ) {
     val snapBehavior = rememberSnapFlingBehavior(lazyListState)
+    val coroutineScope = rememberCoroutineScope()
+    var selectedItemIndex by rememberSaveable { mutableIntStateOf(0) }
 
     LazyColumn(
         modifier = modifier,
@@ -32,9 +42,30 @@ fun VideosListComponent(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(24.dp),
         contentPadding = PaddingValues(top = 75.dp, bottom = 64.dp),
-        userScrollEnabled = false,
     ) {
         items(videos.size) { globalIndex ->
+            LaunchedEffect(lazyListState) {
+                snapshotFlow { lazyListState.layoutInfo }
+                    .collect { layoutInfo ->
+                        val fullyVisibleItems = layoutInfo.visibleItemsInfo
+                            .filter { itemInfo ->
+                                val itemStart = itemInfo.offset
+                                val itemEnd = itemInfo.offset + itemInfo.size
+                                val viewportStart = lazyListState.layoutInfo.viewportStartOffset
+                                val viewportEnd = lazyListState.layoutInfo.viewportEndOffset
+
+                                itemStart >= viewportStart && itemEnd <= viewportEnd
+                            }
+
+                        val firstFullyVisibleItemInfoIndex = fullyVisibleItems.firstOrNull()?.index ?: return@collect
+                        selectedItemIndex = firstFullyVisibleItemInfoIndex
+                        onSelectedItemIndexChanged(firstFullyVisibleItemInfoIndex)
+                        coroutineScope.launch {
+                            lazyListState.animateScrollToItem(firstFullyVisibleItemInfoIndex)
+                        }
+                    }
+            }
+
             val videoPlaying = videoPlayingStates[globalIndex] ?: false
             val playbackPositionMs = videoPositionStates[globalIndex] ?: 0
             val coordinateIndex = coordinateIndicesStates[globalIndex] ?: 0
@@ -51,7 +82,14 @@ fun VideosListComponent(
                     onCoordinateIndexConsumed(globalIndex, consumedIndexOfCoordinate)
                 },
                 onVideoEnded = {
-                    onVideoEnded(globalIndex)
+                    val nextIndex = if (selectedItemIndex + 1 < videos.size) {
+                        selectedItemIndex + 1
+                    } else {
+                        0
+                    }
+                    coroutineScope.launch {
+                        lazyListState.animateScrollToItem(nextIndex)
+                    }
                 },
             )
         }
