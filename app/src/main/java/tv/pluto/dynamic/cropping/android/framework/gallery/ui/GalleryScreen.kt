3ecea47615588549fe0011b53254099b8ece1349
Kamil Pierudzki <kamil.pierudzki@paramount.com>
2024-10-21 10:25:31 +0200

    Update GalleryScreen

--- app/src/main/java/tv/pluto/dynamic/cropping/android/framework/gallery/ui/GalleryScreen.kt
+++ app/src/main/java/tv/pluto/dynamic/cropping/android/framework/gallery/ui/GalleryScreen.kt
@@ -1,22 +1,15 @@
 package tv.pluto.dynamic.cropping.android.framework.gallery.ui
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import tv.pluto.dynamic.cropping.android.framework.Video
@@ -30,36 +23,21 @@ fun GalleryScreen(
     Scaffold { contentPadding ->
         Box(modifier = Modifier
             .padding(contentPadding)
-            .fillMaxSize()) {
+            .fillMaxSize()
+            .padding(16.dp)) {
 
-            val scrollState = rememberScrollState()
-
-            Column(
-                verticalArrangement = Arrangement.spacedBy(24.dp),
+            DynamicCroppingVideoComponent(
+                lifecycleOwner = LocalLifecycleOwner.current,
+                video = video,
+                playbackState = true,
+                playbackPositionMs = 0,
+                onPlaybackPositionChanged = {},
+                onVideoEnded = {},
                 modifier = Modifier
-                    .verticalScroll(scrollState)
-                    .padding(16.dp)) {
-
-                Spacer(modifier = Modifier
-                    .size(130.dp, 130.dp)
-                    .background(Color.Green))
-
-                DynamicCroppingVideoComponent(
-                    lifecycleOwner = LocalLifecycleOwner.current,
-                    video = video,
-                    playbackState = true,
-                    playbackPositionMs = 0,
-                    onPlaybackPositionChanged = {},
-                    onVideoEnded = {},
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .aspectRatio(9f / 16f),
-                )
-
-                Spacer(modifier = Modifier
-                    .size(130.dp, 130.dp)
-                    .background(Color.Magenta))
-            }
+                    .fillMaxWidth()
+                    .aspectRatio(9f / 16f)
+                    .align(Alignment.Center),
+            )
         }
     }
     BackHandler(onBack = onBack)
